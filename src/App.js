b6ae2e1e10e8aea787bb59e4e467703291b26bc1
Erin M <erinfranmc@gmail.com>
2019-11-18 20:02:05 +0000

    SearchBox is separate reusable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
-
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -27,10 +27,9 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="Search monsters"
-          onChange={e => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder="Search monsters..."
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>

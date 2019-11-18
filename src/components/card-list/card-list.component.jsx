09223b775b730c5473645e551d6c16b9778a12f9
Erin M <erinfranmc@gmail.com>
2019-11-18 15:18:48 +0000

    CardList component responsible for listing cards, instead of App.js

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card-list.styles.css";
+
+export const CardList = props => {
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        <h1 key={monster.id}> {monster.name}</h1>
+      ))}
+    </div>
+  );
+};
